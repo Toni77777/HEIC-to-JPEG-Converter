c5d63959f240c5034fd6b4d29242652103cf3b2c
Anton Paliakou <toxa95401@gmail.com>
2026-03-06 11:40:09 +0100

    Initial commit.

---
+++ HEICConverter/HEICConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HEICConverter
+//
+//  Created by Anton Paliakov on 06/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HEICConverter/HEICConverter/HEICConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HEICConverterApp.swift
+//  HEICConverter
+//
+//  Created by Anton Paliakov on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HEICConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
